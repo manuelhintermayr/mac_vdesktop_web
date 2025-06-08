3eae6fc415eb7a3c56a2bf3745f486988164d71f
Manuel Hintermayr <manuel@hintermayr.net>
2025-06-08 13:39:15 +0200

    feat(navigation): add navigation.js for clock functionality and include in desktop.php

--- assets/js/desktop/desktop.js
+++ assets/js/desktop/desktop.js
@@ -132,41 +132,7 @@
     }
     /* Ende - Code fuer FehlerPopUps */
 
-    /* Start - Code fuer die Uhr */
-    function starteUhr() {
-        stelleUhr();
-        setInterval(function () {
-            stelleUhr();
-        }, 1000);
-    }
 
-    function stelleUhr() {
-        var clock = document.getElementById('clock');
-        var d = new Date();
-        var stunde = "";
-        var minutes = "";
-        var seconds = "";
-        if (d.getHours() < 10) {
-            stunde = "0" + d.getHours();
-        }
-        else {
-            stunde = d.getHours();
-        }
-        if (d.getMinutes() < 10) {
-            minutes = "0" + d.getMinutes();
-        }
-        else {
-            minutes = d.getMinutes();
-        }
-        if (d.getSeconds() < 10) {
-            seconds = "0" + d.getSeconds();
-        }
-        else {
-            seconds = d.getSeconds();
-        }
-        clock.innerHTML = stunde + ':' + minutes + ':' + seconds;
-    }
-    /* Ende - Code fuer die Uhr */
 
     /* Start - Codes fuer das Navigation Center */
     function openAndCloseNotificationCenter() {
